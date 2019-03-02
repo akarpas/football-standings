32d0709d43d0554f6c80e94c37ad1eb417f5f067
Andreas Karpasitis <karpasitis@outlook.com>
2019-03-02 15:48:36 +0100

    filter and merge standing

--- src/actions/standings.js
+++ src/actions/standings.js
@@ -1,4 +1,24 @@
+import _ from 'lodash';
+
 const API_KEY = '80d7e188fb9a4f2bb915b7d293d78048';
+
+const mergeStandings = (standings) => {
+  const [ total, home, away ] = standings;
+  const totalSorted = _(total.table).sortBy((item) => item.team.name).value();
+  const homeSorted = _(home.table).sortBy((item) => item.team.name).value();
+  const awaySorted = _(away.table).sortBy((item) => item.team.name).value();
+  const results = totalSorted.map((result, index) => {
+    const { team } = result;
+    return {
+      team: team.name,
+      position: result.position,
+      total: totalSorted[index],
+      home: homeSorted[index],
+      away: awaySorted[index],
+    }
+  })
+  return _(results).sortBy((item) => item.position).value()
+}
 
 export const getStandings = () => dispatch => {
   dispatch({
@@ -14,9 +34,10 @@ export const getStandings = () => dispatch => {
   }).then(response => response.json())
     .then(data => {
       const { standings } = data;
+      const standingsMerged = mergeStandings(standings)
       dispatch({
         type: 'SET_POPULAR_MOVIES',
-        payload: standings
+        payload: standingsMerged
       })
     })
 }
